65f9f36a0c9b90b45a882863d024a4eb7dfe7ef3
FuhYuan Tso <jim@studio27.local>
2020-02-27 01:14:30 -0500

    card component is fixed.  img src wasn't working before due to src value not in a string literal and props weren't interpreted correctly

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 import "./card.styles.css";
 
-export const Card = props => <h1>{props.monster.name}</h1>;
+export const Card = props => (
+  <div className="card-container">
+    <img
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      alt="monsters"
+    />
+    <h1>{props.monster.name}</h1>
+  </div>
+);
